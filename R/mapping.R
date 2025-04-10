e311f535c2e914649c760d21648faf6eaa8636be
diegomsg <108417656+diegomsg@users.noreply.github.com>
2025-04-10 12:54:51 -0300

    Update mapping.R
    leaflet as map package

--- R/mapping.R
+++ R/mapping.R
@@ -1,12 +1,72 @@
 map_object <- function(geodata, layername = "Áreas do KML", alpha = .6) {
-  # stopifnot(
-  #   "Geometria não é válida." = sf::st_is_valid(geodata))
 
-  tm_shape(
-    geodata,
-    name = layername,
-    crs = "auto") +
-    tm_polygons(
-      "Name",
-      fill_alpha = alpha)
+  is_compatible_geom <- function(geom, compatible_types = c("MULTIPOLYGON", "POLYGON")) {
+    geom_type <- sf::st_geometry_type(geom)
+    compat_geom_types <- c("MULTIPOLYGON", "POLYGON")
+
+    stopifnot(
+      "Geometria não é compatível com polígonos." = geom_type %in% compat_geom_types)
+  }
+
+  is_compatible_geom(geodata)
+
+
+  pal <- colorFactor("RdYlBu", geodata$Name)
+  labels <- sprintf(
+    "<b>Nome:</b> %s<br/><b>Descrição:</b> %s",
+    geodata$Name, geodata$Description) |>
+    lapply(htmltools::HTML)
+
+  leaflet(geodata) |>
+    addProviderTiles(providers$CartoDB.Positron, group = "CartoDB") |>
+    addProviderTiles(providers$OpenStreetMap, group = "OpenStreetMap") |>
+    addProviderTiles(providers$Esri.WorldImagery, group = "Satélite") |>
+    addProviderTiles(providers$Esri.WorldTopoMap, group = "Topográfico") |>
+    addPolygons(
+      group = layername,
+      stroke = TRUE,
+      weight = 2,
+      opacity = alpha,
+      color = "white",
+      fill = TRUE,
+      fillOpacity = alpha,
+      fillColor = ~pal(Name),
+      highlightOptions = highlightOptions(
+        weight = 5,
+        color = "#666",
+        fillOpacity = alpha + .2,
+        bringToFront = TRUE),
+      label = labels,
+      labelOptions = labelOptions(
+        textsize = "12px")
+    ) |>
+    addLegend(
+      position = "bottomright",
+      pal = pal,
+      values = ~Name,
+      opacity = alpha + .1,
+      title = layername) |>
+    addLayersControl(
+      baseGroups = c("CartoDB", "OpenStreetMap", "Topográfico", "Satélite"),
+      overlayGroups = layername,
+      position = "topleft")
+}
+
+sizes_table <- function(geodata) {
+  geodata |>
+    rename(
+      Nome = Name,
+      Descrição = Description) |>
+    mutate(
+      geometry = sf::st_make_valid(geometry),
+      "Vértices" = sapply(geometry, \(x) {
+        sf::st_coordinates(x) |>
+          nrow()
+      }),
+      "Área (m²)" = sf::st_area(geometry),
+      "Área (ha)" = units::set_units(`Área (m²)`, "ha"),
+      "Perímetro (m)" = sf::st_perimeter(geometry),
+      "Perímetro (km)" = units::set_units(`Perímetro (m)`, "km")
+    ) |>
+    sf::st_drop_geometry()
 }
