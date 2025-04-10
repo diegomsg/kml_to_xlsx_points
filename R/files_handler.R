18b4753dc78a21a892ffb751ff7dde467bb2b7dd
diegomsg <108417656+diegomsg@users.noreply.github.com>
2025-04-10 13:04:09 -0300

    Update files_handler.R
    no wrapper for writing files

--- R/files_handler.R
+++ R/files_handler.R
@@ -5,17 +5,3 @@ name_auto <- function(id = NULL) {
     sep = "_") |>
   trimws()
 }
-
-export_xlsx <- function(tbl, filename = "file") {
-  writexl::write_xlsx(
-    x = tbl,
-    path = filename,
-    col_names = TRUE)
-}
-
-export_csv <- function(tbl, filename = "file") {
-  readr::write_csv2(
-    x = tbl,
-    file = filename,
-    append = FALSE)
-}
